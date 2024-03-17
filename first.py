d21f67b2a87a6ab3c7b7da23901b78b066a48e9d
saimohith-27 <156565903+saimohith-27@users.noreply.github.com>
2024-03-17 09:31:03 +0530

    Update

---
+++ first.py
@@ -0,0 +1,18 @@
+name = input("Enter your name : ")
+gender = input(" Enter your gender (M/F):")
+if(gender == M or Male or male):
+    print("Welcome Mr.",name)
+elif(gender == F or Female or female)
+    print("Welcome Miss.",name)
+age = int(input("Enter your age: "))
+
+if(age >= 18):
+    print("You are eligible to vote.")
+    print("D'ye want to apply for vote?")
+    print("Y/N")
+    ans = input()
+    if(ans == Y or Yes or yes)
+        Aadhar = int(input ("Enter your Aadhar no. :"))
+        print("")
+else: 
+  print("You are not eligible to vote yet.")
